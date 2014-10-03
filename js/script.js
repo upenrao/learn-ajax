0a0e0d1c89a17ef260dee541baeedbe2683c4598
Ben Jaffe <ben@udacity.com>
2014-10-03 14:38:21 -0700

    Error Handling

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@
 function loadData() {
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
 
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
 
     // load streetview
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
@@ -16,6 +20,9 @@ function loadData() {
     // load nytimes
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
     $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
@@ -25,6 +32,9 @@ function loadData() {
                 '<p>' + article.snippet + '</p>'+
             '</li>');
         };
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
 
@@ -44,7 +54,6 @@ function loadData() {
                 var url = 'http://en.wikipedia.org/wiki/' + articleStr;
                 $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
             };
-
         }
     });
 };
